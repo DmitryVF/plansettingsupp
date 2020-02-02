3f00782ea01f84c827f8b48111392ec907bd216f
DmitryVF <d.v.filimonov@mail.ru>
2020-02-02 22:54:18 +0300

    Updates

---
+++ precache-manifest.9de37a6c819c892a93fa1462a49ca0bd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57045fea55448db268f3d820c2466f0e",
+    "url": "/plansettingsupp/index.html"
+  },
+  {
+    "revision": "59f765afdc73baba06c4",
+    "url": "/plansettingsupp/static/css/2.ff8bd605.chunk.css"
+  },
+  {
+    "revision": "ff5209e2518c582d4bad",
+    "url": "/plansettingsupp/static/css/main.08092650.chunk.css"
+  },
+  {
+    "revision": "59f765afdc73baba06c4",
+    "url": "/plansettingsupp/static/js/2.33cfcca2.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/plansettingsupp/static/js/2.33cfcca2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ff5209e2518c582d4bad",
+    "url": "/plansettingsupp/static/js/main.d7e8b352.chunk.js"
+  },
+  {
+    "revision": "1915bad5fcb40bfe5647",
+    "url": "/plansettingsupp/static/js/runtime-main.bdb6a0d8.js"
+  },
+  {
+    "revision": "93c9e273c534e3b283040b0282178dc0",
+    "url": "/plansettingsupp/static/media/Vector-1.93c9e273.svg"
+  },
+  {
+    "revision": "c79ccbd36b267aa00d5993c0c4df64f7",
+    "url": "/plansettingsupp/static/media/Vector-1.c79ccbd3.svg"
+  },
+  {
+    "revision": "08b2611cb41e23f63e45ed79437a6243",
+    "url": "/plansettingsupp/static/media/Vector.08b2611c.svg"
+  },
+  {
+    "revision": "d5b611366af9e2222b8c43af2ab52195",
+    "url": "/plansettingsupp/static/media/Vector.d5b61136.svg"
+  },
+  {
+    "revision": "714d7f4c7a16c9a13dcbd01e92342a71",
+    "url": "/plansettingsupp/static/media/image8.714d7f4c.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/plansettingsapp/precache-manifest.b3c74aa6fb81d763c6ec33a7bf755f77.js"
+  "/plansettingsupp/precache-manifest.9de37a6c819c892a93fa1462a49ca0bd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/plansettingsapp/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/plansettingsupp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
